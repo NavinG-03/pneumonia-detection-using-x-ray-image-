c2e81458089d9466662b55c69805af92e4b08f75
NavinG-03 <nn4230496@gmail.com>
2025-07-21 10:38:04 +0530

    Update and rename Pneumonia_detection.py to app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
+app = Flask(__name__)
+model = load_model('model/pneumonia_cnn_model.h5')
+
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return "No file uploaded"
+    
+    file = request.files['file']
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(filepath)
+
+    img = image.load_img(filepath, target_size=(150, 150))
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    result = "Pneumonia" if prediction[0][0] > 0.5 else "Normal"
+
+    return render_template('index.html', result=result, img_path=filepath)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
