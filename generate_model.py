66a2b34e73b052ab62dda30576299bb3d48f0c52
NavinG-03 <nn4230496@gmail.com>
2025-07-21 11:07:07 +0530

    Create generate_model.py

---
+++ generate_model.py
@@ -0,0 +1,15 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Save the model to the correct path
+model.save('model/pneumonia_cnn_model.h5')
