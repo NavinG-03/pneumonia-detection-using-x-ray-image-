b4da71c145b0a81f3ab342ba1bce0d869f7332b3
NavinG-03 <nn4230496@gmail.com>
2025-07-26 08:40:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,12 @@ import numpy as np
 import os
 
 app = Flask(__name__)
-
-# Load multiclass-trained model
 model = load_model('model/pneumonia_cnn_model.h5')
 
-# Class names for prediction output
-class_names = ['NORMAL', 'VIRAL', 'BACTERIAL']
-
-# Set upload path
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+classes = ['Normal', 'Bacterial Pneumonia', 'Viral Pneumonia']
 
 @app.route('/')
 def index():
@@ -24,24 +20,18 @@ def index():
 def predict():
     if 'file' not in request.files:
         return "No file uploaded"
-
-    file = request.files['file']
-    if file.filename == '':
-        return "No selected file"
     
+    file = request.files['file']
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(filepath)
 
-    # Preprocess the image
     img = image.load_img(filepath, target_size=(150, 150))
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Make prediction
     prediction = model.predict(img_array)
-    predicted_class = class_names[np.argmax(prediction)]
+    predicted_class = classes[np.argmax(prediction)]
 
-    # Render result
     return render_template('index.html', result=predicted_class, img_path=filepath)
 
 if __name__ == '__main__':
